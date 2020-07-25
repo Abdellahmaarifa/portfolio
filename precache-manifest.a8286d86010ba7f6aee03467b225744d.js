f5777c995aa00c5b96a5b6f40db7887808c93572
Abd Abd <thefitnessfab@gmail.com>
2020-07-25 20:25:31 +0100

    Updates

---
+++ precache-manifest.a8286d86010ba7f6aee03467b225744d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a957b69b824d7de7ba46518dfd148fc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b28b745378056fd31a17",
+    "url": "/portfolio/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "8efa0b33db518d1cfc62",
+    "url": "/portfolio/static/css/main.1a3add85.chunk.css"
+  },
+  {
+    "revision": "b28b745378056fd31a17",
+    "url": "/portfolio/static/js/2.13f15e70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.13f15e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8efa0b33db518d1cfc62",
+    "url": "/portfolio/static/js/main.07bf0434.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "e886dbe2e82a669ec7796573c78b8504",
+    "url": "/portfolio/static/media/Loading-logo.e886dbe2.svg"
+  },
+  {
+    "revision": "0671122171782efa1778d87871083c9e",
+    "url": "/portfolio/static/media/featuredImg.06711221.png"
+  },
+  {
+    "revision": "81b219fefac9bfec5f15dd0e27220603",
+    "url": "/portfolio/static/media/logo.81b219fe.png"
+  },
+  {
+    "revision": "2432c502392f84089350adb7da228e00",
+    "url": "/portfolio/static/media/profile.2432c502.jpg"
+  },
+  {
+    "revision": "7b55574ff739976b792991d4a2346e3c",
+    "url": "/portfolio/static/media/project-background.7b55574f.svg"
+  }
+]);
